15796c4bd7ff8153164e91988bdc889fab122ac2
Muhammed Eraslan <muhammed.ersln@icloud.com>
2024-11-14 19:04:05 +0300

    fix

--- src/app/api/transcribe/route.js
+++ src/app/api/transcribe/route.js
@@ -1,32 +1,34 @@
 import fetch from 'node-fetch';
 import FormData from 'form-data';
 
+export const config = {
+  runtime: 'nodejs', // Vercel sunucu uyumluluğunu sağlar
+};
+
 export async function POST(req) {
   try {
-    // Gelen ses dosyasını alıp Buffer formatına dönüştürelim
+    // Gelen ses dosyasını alıp Buffer formatına dönüştürme
     const audioBuffer = await req.arrayBuffer();
     const audioBufferConverted = Buffer.from(audioBuffer);
 
-    
-    // Ses dosyasını OpenAI API'ye gönder
+    // OpenAI API isteği için FormData hazırlığı
     const formData = new FormData();
     formData.append('file', audioBufferConverted, {
       filename: 'audio.wav',
       contentType: 'audio/wav',
     });
-    formData.append('model', 'whisper-1'); // Whisper modelini burada ekledik
+    formData.append('model', 'whisper-1');
 
     const response = await fetch('https://api.openai.com/v1/audio/transcriptions', {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-        ...formData.getHeaders() // FormData'nın gerekli header'ları otomatik ayarlamasını sağlıyoruzzzzzzzzzzzzzzz
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        ...formData.getHeaders(), // FormData için gerekli header ayarları
       },
       body: formData,
     });
 
     const result = await response.json();
-   
 
     if (response.ok) {
       return new Response(JSON.stringify({ text: result.text }), {
