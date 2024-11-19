d49c3d695681bd774837c72fc6b8cdd4d3d8ece2
Muhammed Eraslan <muhammed.ersln@icloud.com>
2024-11-19 20:18:32 +0300

    update ssml

--- src/app/api/elevenlabs/route.js
+++ src/app/api/elevenlabs/route.js
@@ -14,7 +14,6 @@ export async function POST(req) {
     },
     body: JSON.stringify({
       text,
-      voice_settings: { stability: 0.5, similarity_boost: 0.75, speed: 0.2 }, // Adjust "speed" if available
     }),
   });
 
